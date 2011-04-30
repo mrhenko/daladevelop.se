ea048679ce2f3589c82dd7fa12bc75e7a83b726f
Mikael Jorhult <mikael@jorhult.se>
2011-04-30 22:28:02 +0200

    Link to styles and scripts

--- functions.php
+++ functions.php
@@ -1,7 +1,15 @@
 <?php
-
-function whoIsKing() {
-    return "Emil Österlund";
-}
-
+	add_action('init', 'daladevelop_init');
+	
+	/*
+		Initialize stylesheets and scripts.
+	*/	
+	function daladevelop_init() {
+		if(!is_admin()) { // Enqueue stylsheets and scripts if frontend
+			wp_enqueue_script('script',  get_bloginfo('template_directory') . '/scripts/script.js');
+			wp_enqueue_style('inuit', get_bloginfo('template_directory') . '/styles/inuit.css', array(), '1.0', 'all');
+			wp_enqueue_style('style', get_bloginfo('template_directory') . '/styles/screen.css', array('inuit'), '1.0', 'screen');
+			wp_enqueue_style('print', get_bloginfo('template_directory') . '/styles/print.css', array('inuit'), '1.0', 'print');
+		}
+	}
 ?>
