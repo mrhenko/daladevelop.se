2437d74cd421240812f56aee621c81c62f385abf
Henrik Carlsson <henrik.carlsson@me.com>
2011-05-01 00:08:07 +0200

    Mera AJAx-trix

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,11 @@ $(document).ready(function() {
 		url = url + ' #wrapper article';
 		$('body').append('<div id="tempload" class="hidden"></div>');
 		$('#tempload').load(url, function() {
+			$('div#tempload').children('article').addClass('new');
 			var newpage = $('div#tempload').html();
-			$('#wrapper').children('article').addClass('hidden');
+			$('#wrapper').children('article').addClass('hidden old').removeClass('current');
 			$('#wrapper').append(newpage);
+			$('#wrapper article.new').removeClass('hidden').removeClass('new');
 			$(this).remove();
 		});
 	});
