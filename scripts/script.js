209ba5dfcd7c38895a2e3aa6013b07831dfda40e
Mikael Jorhult <mikael@jorhult.se>
2011-04-30 23:34:41 +0200

    Anonymous function when using jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
-// JavaScript Document
-
-// Hijack links and load via ajax
-$(document).ready(function() {
-	$('a').click(function(event) {
-		event.preventDefault();
-		var url = $(this).attr('href');
-		url = url + ' #wrapper article';
-		$('body').append('<div id="tempload" class="hidden"></div>');
-		$('#tempload').load(url, function() {
-			var newpage = $('div#tempload').html();
-			$('#wrapper').children('article').addClass('hidden');
-			$('#wrapper').append(newpage);
-			$(this).remove();
+(function ($) { 
+	$(document).ready(function() {
+		// Hijack links and load via ajax
+		$('a').click(function(event) {
+			event.preventDefault();
+			var url = $(this).attr('href');
+			url = url + ' #wrapper article';
+			$('body').append('<div id="tempload" class="hidden"></div>');
+			$('#tempload').load(url, function() {
+				var newpage = $('div#tempload').html();
+				$('#wrapper').children('article').addClass('hidden');
+				$('#wrapper').append(newpage);
+				$(this).remove();
+			});
 		});
 	});
-});
+})(jQuery)
