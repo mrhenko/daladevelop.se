eabcd8516c94c0910ad5c5da1fa833986ccf1bde
Henrik Carlsson <henrik.carlsson@me.com>
2011-04-30 23:22:53 +0200

    Första steget mot ajax-laddning är taget

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+
+// Hijack links and load via ajax
+$(document).ready(function() {
+	$('a').click(function(event) {
+		event.preventDefault();
+		var url = $(this).attr('href');
+		url = url + ' #wrapper article';
+		$('body').append('<div id="tempload" class="hidden"></div>');
+		$('#tempload').load(url, function() {
+			var newpage = $('div#tempload').html();
+			$('#wrapper').children('article').addClass('hidden');
+			$('#wrapper').append(newpage);
+			$(this).remove();
+		});
+	});
+});
