b09cc0465e324c41e9a0e837325d76cd14304179
larsemil <emil@larsemil.se>
2011-05-01 18:48:01 +0200

    testar

--- apps/get_posts.php
+++ apps/get_posts.php
@@ -6,8 +6,8 @@ foreach ($_POST['tags'] as $tag_slug) {
   $tag_string .= $tag_slug . ',';
 }
 
-$query = new WP_Query($tag_string);
-
+$query_posts($tag_string);
+/*
 echo '<ul>';
 while ($query->have_posts()) : $query->the_post();
 echo '<li>';
@@ -16,8 +16,7 @@ echo '<li>';
 echo '</li>';
 endwhile;
 echo '</ul>';
+ */
 
-// Bug: These show all posts, not only the ones with the selected tags. 
-#query_posts($tags); 
-#get_template_part('loop','index');
+get_template_part('loop','index');
 ?>
