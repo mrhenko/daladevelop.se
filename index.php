0ba8c8ab7974fe43c13bc1ebbeac775184c3a559
Mikael Jorhult <mikael@jorhult.se>
2011-05-01 02:01:51 +0200

    Use get_template_part for the loop

--- index.php
+++ index.php
@@ -14,13 +14,6 @@ foreach($tags as $tag)
 <h1>Sluttestat</h1>
 <!-- END TEST -->
 
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-	<?php global $post; ?>
-    <article <?php post_class(); ?> id="page-<?php echo $post->post_name; ?>">
-        <header><h2><?php the_title(); ?></h2></header>
-        <?php the_content(); ?>
-    </article>
-<?php endwhile; endif; ?>
-</section>
+<?php get_template_part('loop', 'index'); ?>
 
 <?php get_footer(); ?>
