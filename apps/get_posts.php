a7e84d7db59a8213f3d22e1f02de75f54c2d0dca
larsemil <emil@larsemil.se>
2011-05-01 17:14:06 +0200

    test

--- apps/get_posts.php
+++ apps/get_posts.php
@@ -1,3 +1,22 @@
 <?php
-  print_r($_POST);
+    print_r($_POST);
+    require("../../../../wp-load.php"); // startup the wordpress instance
+
+    //THIS IS ONLY A TEST
+    //THIS TEST SHOULD BE SWITCHED TO THE SCRIPT THAT BUILDS UP THE QUERY STRING DEPENDING ON WHAT IS SENT FROM FORM.
+    $tags = "cat=1";
+    foreach($_POST as $tag)
+    {
+        $tags .= "&tag=$tag";
+    }
+
+    //END OF TEST
+
+
+    //custom query
+   query_posts($tags); 
+
+    //the loop
+    get_template_part('loop','index');
+    
 ?>
