bf7ebc66dcc775982ae60ba4f2ee49a526780bec
Mikael Jorhult <mikael@jorhult.se>
2011-05-01 02:45:44 +0200

    Right class for ajax

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 (function ($) { 
 	$(document).ready(function() {
 		// Hijack links and load via ajax
-		$('#wrapper').delegate('a.ddajax', 'click', function(event) {
+		$('#wrapper').delegate('li.ddajax a', 'click', function(event) {
 			event.preventDefault();
 			
 			var url = $(this).attr('href') + ' #wrapper article';
