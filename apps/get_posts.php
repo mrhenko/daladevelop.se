a83937e27d30d021d4e0093d89c4d9058c8f140e
larsemil <emil@larsemil.se>
2011-05-01 18:51:46 +0200

    Förlåt

--- apps/get_posts.php
+++ apps/get_posts.php
@@ -6,7 +6,7 @@ foreach ($_POST['tags'] as $tag_slug) {
   $tag_string .= $tag_slug . ',';
 }
 
-
+$query = new WP_Query($tag_string);
 echo '<ul>';
 while ($query->have_posts()) : $query->the_post();
 echo '<li>';
