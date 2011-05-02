352446ad793e71bb1af63389d57f98f776d155b7
Henrik Carlsson <henrik.carlsson@me.com>
2011-05-02 15:14:27 +0200

    Tog bort inuit tills vidare

--- functions.php
+++ functions.php
@@ -8,9 +8,12 @@
 	function daladevelop_init() {
 		if(!is_admin()) { // Enqueue stylsheets and scripts if frontend
 			wp_enqueue_script('script',  get_bloginfo('template_directory') . '/scripts/script.js');
-			wp_enqueue_style('inuit', get_bloginfo('template_directory') . '/styles/inuit.css', array(), '1.0', 'all');
-			wp_enqueue_style('style', get_bloginfo('template_directory') . '/styles/screen.css', array('inuit'), '1.0', 'screen');
-			wp_enqueue_style('print', get_bloginfo('template_directory') . '/styles/print.css', array('inuit'), '1.0', 'print');
+			//wp_enqueue_style('inuit', get_bloginfo('template_directory') . '/styles/inuit.css', array(), '1.0', 'all');
+			
+			wp_enqueue_style('style', get_bloginfo('template_directory') . '/styles/screen.css','', '1.0', 'screen');
+			
+			wp_enqueue_style('ajax-slider', get_bloginfo('template_directory') . '/styles/ajax-slider.css');
+			wp_enqueue_style('print', get_bloginfo('template_directory') . '/styles/print.css', '', '1.0', 'print');
         }
 		
 		if (!is_page() || is_single()) {
