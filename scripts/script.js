fb5bceeec8d4fd769cc5959646c171e52125761c
Henrik Carlsson <henrik.carlsson@me.com>
2011-05-02 15:22:45 +0200

    Rensade bort lite oanvänd kod.

--- scripts/script.js
+++ scripts/script.js
@@ -17,11 +17,9 @@ var timer;
 			$('#tempload').load(url, function() {
 				$('div#tempload').children('article').addClass('new');
 				var newpage = $('div#tempload').html();
-                //$('#wrapper').children('article').animate({left:"5000px"},{ duration: "slow" });
-                
 				$('#wrapper').children('article').addClass('old');
-                $('#wrapper').append(newpage);
-                
+				$('#wrapper').append(newpage);
+				
 				$(this).remove();
 				
 				$('#wrapper').animate({
@@ -30,41 +28,28 @@ var timer;
 					$(this).css('left', '0%');
 					$('#wrapper article.old').remove();
 					$('#wrapper article.new').removeClass('hidden').removeClass('new');
-					hide_ajax_loading();
+					$('#dd_ajax_loading').remove();
 				});
 			});
 		});
 
-    // Load blog posts when tag checkboxes are clicked
-    $('#tagcloud').delegate('#tag_box', 'change', function(event) {
-      clearTimeout(timer);
-      timer=setTimeout("post_tag_form()", 1300);
-      $('#posts').animate({opacity: .5});
-      $('#posts').css("background-color: #666;");
+	// Load blog posts when tag checkboxes are clicked
+	$('#tagcloud').delegate('#tag_box', 'change', function(event) {
+  	clearTimeout(timer);
+  	timer=setTimeout("post_tag_form()", 1300);
+  	$('#posts').animate({opacity: .5});
+  	$('#posts').css("background-color: #666;");
 
-    });
+	});
   });
   
   function show_ajax_loading() {
 	$('body').append('<div id="dd_ajax_loading"><p>Laddar...</p><div id="dd_squares"><span id="dd_ajax_loading_one"></span><span id="dd_ajax_loading_two"></span><span id="dd_ajax_loading_three"></span><span id="dd_ajax_loading_four"></span></div></div>');
-	window.setTimeout(function() {
-		$('#dd_ajax_loading_one').addClass('filled');
-	}, 250);
-	window.setTimeout(function() {
-		$('#dd_ajax_loading_two').addClass('filled');
-	}, 500);
-	window.setTimeout(function() {
-		$('#dd_ajax_loading_three').addClass('filled');
-	}, 750);
-	window.setTimeout(function() {
-		$('#dd_ajax_loading_four').addClass('filled');
-	}, 1000);
+	window.setTimeout(function() {$('#dd_ajax_loading_one').addClass('filled');}, 250);
+	window.setTimeout(function() {$('#dd_ajax_loading_two').addClass('filled');}, 500);
+	window.setTimeout(function() {$('#dd_ajax_loading_three').addClass('filled');}, 750);
+	window.setTimeout(function() {$('#dd_ajax_loading_four').addClass('filled');}, 1000);
 }
-
-function hide_ajax_loading() {
-	$('#dd_ajax_loading').remove();
-}
-  
 })(jQuery)
 
 
@@ -75,11 +60,10 @@ function show_tags() {
 function post_tag_form() {
   var data = { 'tags[]': [] };
   $(':checked').each(function() {
-      data['tags[]'].push($(this).val());
+  	data['tags[]'].push($(this).val());
   });
 
   $("#posts").load("/wp-content/themes/daladevelop.se/apps/get_posts.php", data);
   $("#posts").animate({opacity: 1});
   $("#posts").css("background-color: transparent;");
-
 }
