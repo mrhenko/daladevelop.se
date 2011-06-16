ecdfe48d996d340f960f854794b32672df6506e1
larsemil <emil@larsemil.se>
2011-06-16 15:53:01 +0200

    Frack this

---
+++ startsida.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Startsida
+ */
+
+<?php get_header(); ?>
+
+<section id="wrapper">
+    <div id="startstor">
+    
+        <div class="startingress">
+        Senaste tiden har regnet öst ner. Det har bådat gott för daladevelop och vi kommer att... 
+        </div>
+    </div>
+    <div class="startright">
+    
+        <div class="startingress">
+        Ingress2 handlar om lite andra saker, ingen kan veta riktigt.
+        </div>
+    </div>
+
+    <div class="startright">
+        <div class="startingress">
+            Ingress3 handlar om både det ena och det andra. 
+        </div>
+    </div>
+</section>
